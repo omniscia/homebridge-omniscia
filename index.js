33fd4878c4182581786ef68d9b4704008c0ec607
Vivake Gupta <vivake@gmail.com>
2018-10-24 13:00:05 +0000

    X

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let Service, Characteristic, Accessory, uuid;
-let LutronDmxLight, LutronWindowCovering, CoolAccessory;
+let LutronDmxLight, LutronWindowCovering, CoolAccessory, NeoAccessory;
 
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
@@ -19,8 +19,10 @@ module.exports = function (homebridge) {
   LutronDmxLight = require('./lutron-dmxlight.js')(exportedTypes);
   LutronWindowCovering = require('./lutron-windowcovering.js')(exportedTypes);
   CoolAccessory = require('./cool-accessory.js')(exportedTypes);
+  NeoAccessory = require('./neo-accessory.js')(exportedTypes);
 
   homebridge.registerAccessory('homebridge-omniscia', 'LutronDmxLight', LutronDmxLight, true);
   homebridge.registerAccessory('homebridge-omniscia', 'LutronWindowCovering', LutronWindowCovering, true);
-  homebridge.registerAccessory('homebridge-omniscia', 'CoolAccessory', CoolAccessory, true)
+  homebridge.registerAccessory('homebridge-omniscia', 'CoolAccessory', CoolAccessory, true);
+  homebridge.registerAccessory('homebridge-omniscia', 'NeoAccessory', NeoAccessory, true);
 };
